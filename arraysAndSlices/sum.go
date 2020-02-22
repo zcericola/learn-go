d9815add348fe8b73c9bbb728454536ee3b5beef
Zac Cericola <zcericola@gmail.com>
2020-02-22 17:11:38 -0600

    finished arrays and slices

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -9,5 +9,31 @@ func Sum(numbers []int) int {
 	return sum
 }
 
+//SumAll will take any number of arrays and return the total for each
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int //slice of an array with ints
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers)) // sum a slice and then append it to the slice of slices
+	}
+	return sums
+
+}
+
+//SumAllTails will take any number of arrays and return the total for the tail of each array
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			//add a zero to the sums array if it is empty
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:] //slice the numbers arr starting at index 1 to the end of the array
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+
+}
+
 func main() {
 }
