c37d6b0bf9e7327a413ce495da1e0dc183275e8f
Zac Cericola <zcericola@gmail.com>
2020-02-22 22:05:17 -0600

    finished structsMethodsAndInterfaces chapter

--- structsMethodsInterfaces/shapes.go
+++ structsMethodsInterfaces/shapes.go
@@ -1,9 +1,44 @@
 package main
 
+import "math"
+
+//Shape allows to calc the area for any shape
+type Shape interface {
+	Area() float64
+}
+
 //Rectangle represents the shape rectangle with Width and Length properties
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+//Area is a method on the Rectangle struct
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+//Circle represents the shape circle with a radius property
+type Circle struct {
+	Radius float64
+}
+
+//Area is a method on the Circle struct
+func (c Circle) Area() float64 {
+	rSquared := c.Radius * c.Radius
+	return math.Pi * rSquared
+
+}
+
+//Triangle represents a triangle with base and height
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+//Area is method on the Triangle struct
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * .5
 }
 
 //Perimeter returns the perimeter length of a shape
--- structsMethodsInterfaces/shapes_test.go
+++ structsMethodsInterfaces/shapes_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
@@ -12,10 +14,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	/*areaTests is an anonymous struct which is an array of structs each containing a name, shape, and the hasArea property
+	this is an example of table based tests which make it easy to test multiple cases
+	, but are cumbersome when only simple testing is needed
+	tests should act as an assertion of truth about the code, not as a sequence of operations*/
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		//naming struct fields is optional but helps readability
+		{shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+		}
 	}
 }
